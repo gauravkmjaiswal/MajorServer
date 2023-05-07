8e44c2cbc29462462f352ce448f8a884e28d364e
Gaurav Jaiswal <gauravkmjaiswal.com>
2023-05-07 17:40:56 +0530

    gas limit

--- src/transferToken.js
+++ src/transferToken.js
@@ -266,7 +266,7 @@ async function transferTokens(recipientAddress, amount ,senderPrivateAddress) {
 
   // const gasLimit = await tokenContract.estimateGas.transfer(recipientAddress, amount) ;
   // const tx = await tokenContract.transfer(recipientAddress, amount, {  gasLimit: gasLimit * 4  });
-  const gasPrice = ethers.utils.parseUnits('1000', 'gwei'); // replace with your desired gas price
+  const gasPrice = ethers.utils.parseUnits('10', 'gwei'); // replace with your desired gas price
   const gasLimit = await tokenContract.estimateGas.transfer(recipientAddress, amount);
   const tx = await tokenContract.transfer(recipientAddress, amount, { gasLimit:gasLimit*20, gasPrice });
   console.log(tx)
